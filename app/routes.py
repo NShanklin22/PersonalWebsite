b1594dcf90b19f58f4060fbbb133968d3eb24f28
NShanklin22 <nshanklin22@yahoo.com>
2023-01-12 17:17:57 -0500

    Following flask guide for website

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,44 @@
 from flask import render_template
+from flask import flash
+from flask import redirect, url_for, request
+from werkzeug.urls import url_parse
+from flask_login import logout_user
 from app import app
-from app.forms import LoginForm
+from app import db
+from flask_login import current_user, login_user
+from app.models import User
+from app.forms import LoginForm, RegistrationForm
+from flask_login import login_required
+from app.forms import EditProfileForm
+from datetime import  datetime
 
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+# Route for the edit profile page
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
+
+# Route for the index page
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -16,14 +51,58 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template("index.html", title='Home Page', posts=posts)
 
-
+# Route for the login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+    # Check if the user is already authenticated, if yes then redirect to index
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+    # Create a login form object defined in another file
+    form = LoginForm()
+    # If a valid for is submited, verify the context, login, then redirect to index
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    # If a form is not submitted redirect to the index page
     return render_template('login.html', title='Sign In', form=form)
+
+# Route called when user clicks logout
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+# Route for the page to register a new user
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+# Page for each unique user
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
